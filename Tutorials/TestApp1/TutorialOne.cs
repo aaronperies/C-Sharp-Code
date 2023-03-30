71dd9d115b4c48bc4221e12e70e700b57d3124af
Aaron Peries <50733765+aaronperies@users.noreply.github.com>
2023-03-30 14:09:07 +0530

    Add files via upload
    First tutorial on C#, as with every other language, "Hello World!"

---
+++ Tutorials/TestApp1/TutorialOne.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Tutorial
+{
+    class TutorialOne
+    {
+        public static void Main(string[] args)
+        {
+            String name = "Aaron!";
+            Console.WriteLine("Hello World! My name is " + name + ", this is my first time coding with C#");
+            
+            int age = 23;
+            Console.WriteLine("I am " + age + " years old");
+        }
+    }    
+}
