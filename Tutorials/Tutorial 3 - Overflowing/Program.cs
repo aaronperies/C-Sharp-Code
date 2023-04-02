0c219401cc1cd88e48af943201a6f3a0b4846523
Aaron Peries <50733765+aaronperies@users.noreply.github.com>
2023-04-02 18:44:35 +0530

    Add files via upload
    Program written to display how overflowing can take place within certain variable types, especially bytes

---
+++ Tutorials/Tutorial 3 - Overflowing/Program.cs
@@ -0,0 +1,19 @@
+﻿//this tutorial is created to demonstrate the purposes of the checked block in C#
+//it helps with catching errors in possible code blocks
+
+//for example, let's assume I wanted to create a byte variable
+byte sequence = 255;
+
+//now I will try adding + 1 to that variable and printing out the result
+Console.WriteLine(sequence += 1);
+
+//so why do we get 0 as the output? Because we have overflown the valid data
+//range for the byte type variable which is 255
+
+//now we'll try executing the same code but within a checked block
+checked
+{
+    byte number = 255;
+    Console.WriteLine(number += 1);
+}
+//this checked block throws a overflow exception which let's us know what the error is
